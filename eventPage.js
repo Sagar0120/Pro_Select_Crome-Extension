10c02589bd856d85d586c25c03254422b1267edb
Sagar <sagarbhanushali0120@gmail.com>
2021-09-10 22:51:49 +0530

    Translate extenstion added

--- eventPage.js
+++ eventPage.js
@@ -4,14 +4,7 @@ let menuItem = {
     "contexts": ["selection"]
 };
 
-let menuItem2 = {
-    "id": "Wikit",
-    "title": "Wikit",
-    "contexts": ["selection"]
-};
-
 chrome.contextMenus.create(menuItem);
-chrome.contextMenus.create(menuItem2);
 
 chrome.contextMenus.onClicked.addListener(function(clickData){ 
     if (clickData.menuItemId == "Speak" && clickData.selectionText){        
@@ -22,15 +15,55 @@ chrome.contextMenus.onClicked.addListener(function(clickData){
     }
 });
 
+// for wiki
 
+let menuItem2 = {
+    "id": "Wikit",
+    "title": "Wikit",
+    "contexts": ["selection"]
+};
+
+chrome.contextMenus.create(menuItem2);
 
 function fixedEncodeURI (str) {
     return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
 }
 
+function fixedEncodeURIComponent (str) {
+    return encodeURIComponent(str).replace(/[!'()*]/g, function(c) {
+      return '%' + c.charCodeAt(0).toString(16);
+    });
+  }
+
 chrome.contextMenus.onClicked.addListener(function(clickData){   
     if (clickData.menuItemId == "Wikit" && clickData.selectionText){    
         var wikiUrl = "https://en.wikipedia.org/wiki/" + fixedEncodeURI(clickData.selectionText);
+        var createData = {
+            "url": wikiUrl,
+            "type": "popup",
+            "top": 5,
+            "left": 5,
+            "width": screen.availWidth/2,
+            "height": screen.availHeight/2
+        };              
+        chrome.windows.create(createData, function(){});        
+    }
+});
+
+// to translate
+
+let menuItem3 = {
+    "id": "translate",
+    "title": "Translate",
+    "contexts": ["selection"]
+};
+
+chrome.contextMenus.create(menuItem3);
+
+chrome.contextMenus.onClicked.addListener(function(clickData){   
+    if (clickData.menuItemId == "translate" && clickData.selectionText){    
+        let convert = JSON.stringify(clickData.selectionText)
+        var wikiUrl = "https://translate.google.com/?sl=en&tl=gu&text=" + convert + "&op=translate";
         var createData = {
             "url": wikiUrl,
             "type": "popup",
