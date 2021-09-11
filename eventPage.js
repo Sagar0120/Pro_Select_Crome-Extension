225950ee59caaf259b74e97e6324ba3fc4abdf4b
Sagar <sagarbhanushali0120@gmail.com>
2021-09-11 11:04:34 +0530

    all feachers added

--- eventPage.js
+++ eventPage.js
@@ -1,10 +1,11 @@
-let menuItem = {
+// for speak
+let menuItem1 = {
     "id": "Speak",
     "title": "Speak",
     "contexts": ["selection"]
 };
 
-chrome.contextMenus.create(menuItem);
+chrome.contextMenus.create(menuItem1);
 
 chrome.contextMenus.onClicked.addListener(function(clickData){ 
     if (clickData.menuItemId == "Speak" && clickData.selectionText){        
@@ -29,16 +30,10 @@ function fixedEncodeURI (str) {
     return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
 }
 
-function fixedEncodeURIComponent (str) {
-    return encodeURIComponent(str).replace(/[!'()*]/g, function(c) {
-      return '%' + c.charCodeAt(0).toString(16);
-    });
-  }
-
 chrome.contextMenus.onClicked.addListener(function(clickData){   
     if (clickData.menuItemId == "Wikit" && clickData.selectionText){    
-        var wikiUrl = "https://en.wikipedia.org/wiki/" + fixedEncodeURI(clickData.selectionText);
-        var createData = {
+        let wikiUrl = "https://en.wikipedia.org/wiki/" + fixedEncodeURI(clickData.selectionText);
+        let createData = {
             "url": wikiUrl,
             "type": "popup",
             "top": 5,
@@ -63,9 +58,62 @@ chrome.contextMenus.create(menuItem3);
 chrome.contextMenus.onClicked.addListener(function(clickData){   
     if (clickData.menuItemId == "translate" && clickData.selectionText){    
         let convert = JSON.stringify(clickData.selectionText)
-        var wikiUrl = "https://translate.google.com/?sl=en&tl=gu&text=" + convert + "&op=translate";
-        var createData = {
-            "url": wikiUrl,
+        let TranslateUrl = "https://translate.google.com/?sl=en&tl=hi&text=" + convert + "&op=translate";
+        let createData = {
+            "url": TranslateUrl,
+            "type": "popup",
+            "top": 5,
+            "left": 5,
+            "width": screen.availWidth/2,
+            "height": screen.availHeight/2
+        };              
+        chrome.windows.create(createData, function(){});        
+    }
+});
+
+// for youtube search
+
+
+let menuItem4 = {
+    "id": "youtube",
+    "title": "Youtube",
+    "contexts": ["selection"]
+};
+
+chrome.contextMenus.create(menuItem4);
+
+chrome.contextMenus.onClicked.addListener(function(clickData){   
+    if (clickData.menuItemId == "youtube" && clickData.selectionText){    
+        let convert = JSON.stringify(clickData.selectionText)
+        let YoutubeUrl = "https://www.youtube.com/results?search_query=" + convert;
+        let createData = {
+            "url": YoutubeUrl,
+            "type": "popup",
+            "top": 5,
+            "left": 5,
+            "width": screen.availWidth/2,
+            "height": screen.availHeight/2
+        };              
+        chrome.windows.create(createData, function(){});        
+    }
+});
+
+// for images
+
+let menuItem5 = {
+    "id": "images",
+    "title": "Google Images",
+    "contexts": ["selection"]
+};
+
+chrome.contextMenus.create(menuItem5);
+
+chrome.contextMenus.onClicked.addListener(function(clickData){   
+    if (clickData.menuItemId == "images" && clickData.selectionText){    
+        let convert = JSON.stringify(clickData.selectionText)
+        let ImageUrl = "https://www.google.com/search?q=" + convert + "+images&source=lnms&tbm=isch&sa=X&sqi=2&ved=1536&bih=722";
+        let createData = {
+            "url": ImageUrl,
             "type": "popup",
             "top": 5,
             "left": 5,
